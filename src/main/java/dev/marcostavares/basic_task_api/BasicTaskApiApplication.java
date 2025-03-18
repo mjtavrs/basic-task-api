e5cbc6afd1705cc9478c066832b9e98ac25e7a9a
Marcos Tavares <mjtdes.md@gmail.com>
2025-03-18 10:13:43 -0300

    Initial structure

---
+++ src/main/java/dev/marcostavares/basic_task_api/BasicTaskApiApplication.java
@@ -0,0 +1,13 @@
+package dev.marcostavares.basic_task_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcostavares/basic_task_api/BasicTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcostavares.basic_task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
